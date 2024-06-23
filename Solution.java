6a4e1d9a86ab4270c72eaef09b98e6ba98687169
SumanthJella <56115740+SumanthJella@users.noreply.github.com>
2024-06-23 23:04:38 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,24 @@
+public class Solution{
+	public static int climbStairs(int n)
+	{
+		if(n==1) return 1;
+		if(n==2) return 2;
+		int i=1;
+		int j=2;
+		int count = 0;
+		while(i<=n-2)
+		{
+			count = j + i;
+			i = j;
+			j = count;
+		}
+		return count;
+	}
+	
+	public static void main(String[] args)
+	{
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		System.out.println("Number of ways to reach the top of stairs ="+climbStairs(n));
+	}
+}
